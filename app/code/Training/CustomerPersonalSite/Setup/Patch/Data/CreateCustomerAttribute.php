c146307c96276da7547dbf10524f4291ba2ebd73
aleks <webmaster.avn@gmail.com>
2021-10-27 21:37:07 +0300

    5.3.

---
+++ app/code/Training/CustomerPersonalSite/Setup/Patch/Data/CreateCustomerAttribute.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Training\CustomerPersonalSite\Setup\Patch\Data;['form_code' => 'customer_account_create', 'attribute_id' => $attributeId],
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+
+class CreateCustomerAttribute implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    public function apply()
+    {
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $attributeName = 'personal_site';
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, $attributeName, [
+            'type' => 'static',
+            'label' => 'Personal Site URL',
+            'input' => 'text',
+// add url validation rule twice in different format for utilizing by different forms
+            'validate_rules' => '{"max_text_length":200,"input_validation":"url","validate-url":true}',
+            'required' => false,
+            'system' => false,
+            'user_defined' => true,
+            'group' => 'General',
+            'is_used_in_grid' => true,
+            'is_visible_in_grid' => true,
+            'is_filterable_in_grid' => true,
+        ]);
+        $attributeId = $customerSetup->getAttributeId(\Magento\Customer\Model\Customer::ENTITY,
+            $attributeName);
+        $this->moduleDataSetup->getConnection()->insertMultiple($this->moduleDataSetup->getTable('customer_form_attribute'),
+            [
+                ['form_code' => 'adminhtml_customer', 'attribute_id' => $attributeId],
+                ['form_code' => 'customer_account_edit', 'attribute_id' => $attributeId]
+            ]);
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * Rollback all changes, done by this patch
+     *
+     * @return void
+     */
+    public function revert()
+    {
+    }
+}
